2e962ba9d186f8c95cd13d70545b616da30432e8
fansor <dlckdduq1107@gmail.com>
2021-08-08 19:20:34 +0900

    first_complete_notificator

--- privateOrSettingData.js
+++
@@ -1,8 +0,0 @@
-
-const token = "xoxp-2084591056242-2283604534340-2352072875525-da56294481977eb1d82d78ce415e9f1f";//발급받은 토큰 번호를 넣어준다.
-const lastest = 3;
-const channelForSend = "출첵알리미";
-const checkInChannel = "check_in_check_out";
-
-
-module.exports = {token,lastest,channelForSend,checkInChannel}
